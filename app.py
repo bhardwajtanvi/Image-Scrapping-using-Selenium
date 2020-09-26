a68bce11b1e046dbcd7ebb3010a9275a14322688
unknown <tanvibhardwaj37@gmail.com>
2020-09-27 01:06:31 +0530

    make it better

--- app.py
+++ app.py
@@ -28,10 +28,8 @@ def display():
             print(type(nums))
             search_and_download(search_term=name, driver_path=DRIVER_PATH,number_images=nums) # method to download images
             makezip()
-            return render_template("result.html",number=nums)
-        else:
-            return render_template("home.html")
-
+            return render_template("result.html")
+        
 
 if __name__ == "__main__":
     app.run(debug=False)
