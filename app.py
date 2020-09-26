5843264ea44de7f434c29ee0ccad6f2e2d73297f
unknown <tanvibhardwaj37@gmail.com>
2020-09-27 01:25:46 +0530

    make it better

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def display():
         
 
 if __name__ == "__main__":
+    
     app.run(debug=False)
--- img_scrapper.py
+++ img_scrapper.py
@@ -12,7 +12,9 @@ chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
+
     def scroll_to_end(wd):
+
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(sleep_between_interactions)
 
@@ -27,6 +29,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
     image_count = 0
     results_start = 0
     while image_count < max_links_to_fetch:
+
         scroll_to_end(wd)
 
         # get all image thumbnail results
@@ -37,6 +40,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
             f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
 
         for img in thumbnail_results[results_start:number_results]:
+
             # try to click every thumbnail such that we can get the real image behind it
             try:
                 img.click()
@@ -47,6 +51,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
             # extract image urls
             actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
             for actual_image in actual_images:
+
                 if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
                     image_urls.add(actual_image.get_attribute('src'))
 
@@ -55,11 +60,10 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
             if len(image_urls) >= max_links_to_fetch:
                 print(f"Found: {len(image_urls)} image links, done!")
                 break
-        else:
-            print("Found:", len(image_urls),
-                  "image links, looking for more ...")
-            time.sleep(30)
-            return
+            else:
+                print("Found:", len(image_urls),"image links, looking for more ...")
+                time.sleep(30)
+                return
             load_more_button = wd.find_element_by_css_selector(".mye4qd")
             if load_more_button:
                 wd.execute_script("document.querySelector('.mye4qd').click();")
