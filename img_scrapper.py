a3a038210b32ff2efbeef32c163c65225767d2fa
unknown <tanvibhardwaj37@gmail.com>
2020-09-26 23:58:54 +0530

    make it better

--- img_scrapper.py
+++ img_scrapper.py
@@ -5,13 +5,11 @@ import requests
 from selenium import webdriver
 import shutil
 chrome_options = webdriver.ChromeOptions()
-
-chrome_options.binary_location= os.environ.get("GOOGLE_CHROME_BIN")
 # chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 # chrome_options.add_argument('window-size=1920,1080');
 chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
     def scroll_to_end(wd):
