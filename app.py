5dd2a833c7375277f1141593e29f4bfb58a4cbfb
unknown <tanvibhardwaj37@gmail.com>
2020-09-29 02:30:03 +0530

    make it better

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def delete(file):
     return True
 
 
-
-if __name__ == "__main__":
-    
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
