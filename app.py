1c4fabeb2dbd8ed3d00a59c3144969f712f6d47d
unknown <tanvibhardwaj37@gmail.com>
2020-10-01 00:43:20 +0530

    make it better

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask,redirect,url_for,render_template,request,send_file,send_
 import img_scrapper
 from img_scrapper import search_and_download
 from img_scrapper import makezip
+from img_scrapper import delunwantedzip
 import os
 from os import path
 import time
 import requests
 from selenium import webdriver
 import shutil
-
+import glob
 
 
 
@@ -27,23 +28,23 @@ def display():
 
             name = request.form['Item-Name']
             num= request.form['Item-Number']
-            nums= abs(int((num)))
+            nums= int(num)
             # print(type(nums))
             search_and_download(search_term=name,number_images=nums) # method to download images
             output=makezip(search_term=name,number_images=nums)
             return render_template("result.html",value=output)
-        return redirect(url_for('home'))
+        
     except Exception as e:
         return render_template("handler.html", error = str(e))
 
 
 @app.route('/delete/<file>',methods=['POST'])
 def delete(file):
-    t=3600
+    t=30
     while t:
         mins, secs = divmod(t, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        # print(timeformat, end='\r')
+        print(timeformat, end='\r')
         time.sleep(1)
         t -= 1
     os.remove("static/"+file)
--- img_scrapper.py
+++ img_scrapper.py
@@ -3,6 +3,7 @@ import time
 import requests
 from selenium import webdriver
 import shutil
+import glob
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -33,7 +34,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd=driver, sleep_betwe
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
         number_results = len(thumbnail_results)
 
-        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+        # print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
 
         for img in thumbnail_results[results_start:number_results]:
             # try to click every thumbnail such that we can get the real image behind it
@@ -52,10 +53,10 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd=driver, sleep_betwe
             image_count = len(image_urls)
 
             if len(image_urls) >= max_links_to_fetch:
-                print(f"Found: {len(image_urls)} image links, done!")
+                # print(f"Found: {len(image_urls)} image links, done!")
                 break
         else:
-            print("Found:", len(image_urls), "image links, looking for more ...")
+            # print("Found:", len(image_urls), "image links, looking for more ...")
             time.sleep(30)
             
             load_more_button = wd.find_element_by_css_selector(".mye4qd")
@@ -66,7 +67,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd=driver, sleep_betwe
 
         # move the result startpoint further down
         results_start = len(thumbnail_results)
-        print(image_urls)
+        # print(image_urls)
     return image_urls
 
 def persist_image(folder_path:str,url:str, counter):
@@ -74,23 +75,25 @@ def persist_image(folder_path:str,url:str, counter):
         image_content = requests.get(url).content
 
     except Exception as e:
-        print(f"ERROR - Could not download {url} - {e}")
+        return e
+        # print(f"ERROR - Could not download {url} - {e}")
 
     try:
         f = open(os.path.join(folder_path, 'jpg' + "_" + str(counter) + ".jpg"), 'wb')
         f.write(image_content)
         f.close()
-        print(f"SUCCESS - saved {url} - as {folder_path}")
+        # print(f"SUCCESS - saved {url} - as {folder_path}")
     except Exception as e:
-        print(f"ERROR - Could not save {url} - {e}")
+        return e
+        # print(f"ERROR - Could not save {url} - {e}")
 
 def search_and_download(search_term, number_images, driver=driver, target_path='./images'):
     
     global glob_time 
-    glob_time = str(time.time())
-    # print("gt of search",glob_time)
+    glob_time_ini = time.time()
+    glob_time = str(glob_time_ini)
     target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' '))+'_'+str(number_images)+glob_time) # make the folder name inside images with the search string
-
+    delunwantedimgs()
     
 
     if not os.path.exists(target_folder):
@@ -105,11 +108,28 @@ def search_and_download(search_term, number_images, driver=driver, target_path='
         persist_image(target_folder, elem, counter)
         counter += 1
 
+def delunwantedzip():
+    global glob_time
+    glob_time_float = float(glob_time)
+    for name in glob.glob('static/*_*.zip'):
+        if os.stat(name).st_mtime < glob_time_float - 5*60:
+            if os.path.isfile(name):
+                os.remove(name)
+                
+def delunwantedimgs():
+    global glob_time
+    glob_time_float = float(glob_time)
+    for name in glob.glob('images/*_*'):
+        if os.stat(name).st_mtime < glob_time_float - 5*60:
+            if os.path.isdir(name):
+                shutil.rmtree(name)
+        
+
+
 def makezip(search_term,number_images):
+    delunwantedzip()
     target_path='./images'
     global glob_time 
-    
-    # print("gt",glob_time)
     fold= os.path.join(target_path, '_'.join(search_term.lower().split(' '))+'_'+str(number_images)+glob_time)
     zip_name= search_term.lower()+'_'+str(number_images)+glob_time+'.zip'
     shutil.make_archive("static/"+search_term.lower()+'_'+str(number_images)+glob_time, "zip", fold)
